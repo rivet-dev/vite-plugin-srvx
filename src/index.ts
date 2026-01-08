c8486ca7cd0e35b4a01ddfcc8af0a6c9304f0eca
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2026-01-08 01:28:48 +0100

    fix workflows

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import { srvx } from './srvx.js'
+import { srvx } from "./srvx";
 
-// Unified plugin (recommended)
-export { srvx, defaultSrvxOptions } from './srvx.js'
-export type { SrvxOptions } from './srvx.js'
-
+export type { BuildOptions } from "./build";
+export { defaultBuildOptions, srvxBuild } from "./build";
+export type { ClientBuildOptions } from "./client-build";
+export { clientBuild, defaultClientBuildOptions } from "./client-build";
+export type { DevServerOptions } from "./dev-server";
 // Individual plugins (for advanced use cases)
-export { devServer, defaultOptions } from './dev-server.js'
-export type { DevServerOptions } from './dev-server.js'
-export { clientBuild, defaultClientBuildOptions } from './client-build.js'
-export type { ClientBuildOptions } from './client-build.js'
-export { srvxBuild, defaultBuildOptions } from './build.js'
-export type { BuildOptions } from './build.js'
+export { defaultOptions, devServer } from "./dev-server";
+export type { SrvxOptions } from "./srvx";
+// Unified plugin (recommended)
+export { defaultSrvxOptions, srvx } from "./srvx";
 
 // Default export is the unified plugin
-export default srvx
+export default srvx;
